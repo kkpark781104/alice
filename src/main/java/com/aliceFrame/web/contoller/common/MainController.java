f83ad20d52a1379b905d78a8b7667ac579c6fe4f
kkpark <kkpark@DESKTOP-B3OKFAA>
2020-01-14 17:55:29 +0900

    ^^

---
+++ src/main/java/com/aliceFrame/web/contoller/common/MainController.java
@@ -0,0 +1,24 @@
+package com.aliceFrame.web.contoller.common;
+
+import java.util.Locale;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+@Controller
+public class MainController {
+
+	/**
+	 * 메인화면 호출
+	 * @param locale
+	 * @param model
+	 * @return
+	 */
+	@RequestMapping(value = "/main", method = {RequestMethod.GET, RequestMethod.POST}) 
+	public String login(Locale locale, Model model) { 
+		System.out.println("메인");
+		return "main/main"; 
+	}
+}
---
+++ src/main/webapp/WEB-INF/views/main/main.jsp
@@ -0,0 +1,14 @@
+<%@ page language="java" contentType="text/html; charset=UTF-8" pageEncoding="UTF-8"%>
+<%@ taglib uri="http://java.sun.com/jsp/jstl/core" prefix="c" %>
+<%@ page session="false" %>
+<html>
+<head>
+	<title>Main</title>
+</head>
+<body>
+<h1>
+	Main
+</h1>
+
+</body>
+</html>
